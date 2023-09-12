bfaa64d6d1a1dfab217c36d125ccbee1ae5d014d
Prathmdeore <144782372+Prathmdeore@users.noreply.github.com>
2023-09-12 14:38:48 +0530

    Create pp.c

---
+++ pp.c
@@ -0,0 +1,11 @@
+#include<iostream.h>
+using namespace std ;
+int main()
+{
+  int roll;
+  cout<<"enter your roll number"<<endl;
+  cin>>roll;
+  cout<<"displaying your details"<<endl;
+  cout<<roll<<endl;
+
+}
